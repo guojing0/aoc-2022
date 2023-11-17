771f2096616c59c8cb8eef853edc828ce455ef9e
Jing Guo <dev.guoj@gmail.com>
2023-11-17 22:48:46 +0100

    third problem

---
+++ aoc_3.rb
@@ -0,0 +1,15 @@
+hash_table = ('a'..'z').zip(1..26).to_h
+uppercase_hash = ('A'..'Z').zip(27..52).to_h
+hash_table.update(uppercase_hash)
+
+problem_input = File.readlines('aoc-3.txt')
+
+def first_problem(input, hash_table)
+  input.sum do |bag|
+    fst, snd = bag.chars.each_slice(bag.length / 2).map(&:join)
+    common_char = (fst.chars & snd.chars).first
+    hash_table[common_char]
+  end
+end
+
+p first_problem(problem_input, hash_table)
